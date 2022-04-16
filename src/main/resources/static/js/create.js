55d3fd28b8883e6002656dad82b61e5ef576d37d
Jennifer <jennifer.zaminer@gmail.com>
2022-04-16 15:46:09 +0200

    create Event view implemented

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -1,10 +1,3 @@
-"use strict";
-
-function openWindow() {
-    let window = Window.open("create.html", "CreateAppointment");
-    
-}
-
 document.getElementById("submit").addEventListener("click", createEvent)
 
 function createEvent() {
