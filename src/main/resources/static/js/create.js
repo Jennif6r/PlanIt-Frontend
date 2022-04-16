d13b641904f56710afd31390d659c78ee87a727c
Jennifer <jennifer.zaminer@gmail.com>
2022-04-16 16:44:24 +0200

    read input and save in variables

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -1,5 +1,43 @@
+let eventName;
+let startDate;
+let startTime;
+let endDate;
+let endTime;
+let category;
+let flexible = false;
+
 document.getElementById("submit").addEventListener("click", createEvent)
+document.getElementById("inputflexible").addEventListener("click", changeFlexible)
 
 function createEvent() {
-    console.log("test");
+    readInput();
+    checkCorrecktness()
+}
+
+function changeFlexible() {
+    if(flexible) {
+        flexible = false;
+    } else {
+        flexible = true;
+    }
+}
+
+function readInput(){
+    eventName = document.getElementById("inputname").value;
+    console.log(eventName);
+    startDate = document.getElementById("inputstartdate").value;
+    console.log(startDate);
+    startTime = document.getElementById("inputstarttime").value;
+    console.log(startTime);
+    endDate = document.getElementById("inputenddate").value;
+    console.log(endDate);
+    endTime = document.getElementById("inputendtime").value;
+    console.log(endTime);
+    category = document.getElementById("inputcategory").value;
+    console.log(category);
+    console.log(flexible);
+}
+
+function checkCorrecktness(){
+    
 }
