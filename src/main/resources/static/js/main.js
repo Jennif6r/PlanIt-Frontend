b46d808327599b3109012379721b8a19befe7be5
Jennifer <jennifer.zaminer@gmail.com>
2022-04-17 13:13:35 +0200

    correct words

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,1 @@
 "use strict";
-
-function openWindow() {
-    let window = Window.open("create.html", "CreateAppointment");
-    
-}
-
-document.getElementById("submit").addEventListener("click", createEvent)
-
-function createEvent() {
-    console.log("test");
-}
