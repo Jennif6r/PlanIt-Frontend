8fb334687cc3a5334fccc2768fb61b84c7de6354
Jennifer <jennifer.zaminer@gmail.com>
2022-04-19 11:28:57 +0200

    changes in Methods

--- src/main/java/PlanIt/PlanitFrontend/controller/PlanItController.java
+++ src/main/java/PlanIt/PlanitFrontend/controller/PlanItController.java
@@ -6,18 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import main.AppointmentJSON;
+import main.AppointmentModel;
 
 @RestController
 public class PlanItController {
 	
 	@RequestMapping(method=RequestMethod.POST, value="/test")
-	public String getTest(@ModelAttribute("startdate") String start, @ModelAttribute("enddate") String end) {
+	public String getAppointmentsInWeek(@ModelAttribute("startdate") String start, @ModelAttribute("enddate") String end) {
+		
 		return start + " " + end;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/create")
-	public AppointmentJSON createAppointment(@RequestBody AppointmentJSON appointment){
+	public AppointmentModel createAppointment(@RequestBody AppointmentModel appointment){
 		// der Code
 		return appointment;
 	}
