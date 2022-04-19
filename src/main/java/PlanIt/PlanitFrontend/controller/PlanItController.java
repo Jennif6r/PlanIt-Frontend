a308592713b4530e6e81fa7f09edf10c0eb7f9e8
PatrickFreyy <patrick.langenalb@gmail.com>
2022-04-19 12:00:07 +0200

    get Appointments in Week implemented

--- src/main/java/PlanIt/PlanitFrontend/controller/PlanItController.java
+++ src/main/java/PlanIt/PlanitFrontend/controller/PlanItController.java
@@ -1,5 +1,10 @@
 package PlanIt.PlanitFrontend.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,19 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.AppointmentModel;
+import main.*;
 
 @RestController
 public class PlanItController {
+	Persistence fp = new FilePersistence();
 	
 	@RequestMapping(method=RequestMethod.POST, value="/test")
-	public String getAppointmentsInWeek(@ModelAttribute("startdate") String start, @ModelAttribute("enddate") String end) {
-		
-		return start + " " + end;
+	public List<Appointment> getAppointmentsInWeek(@ModelAttribute("startdate") String start, @ModelAttribute("enddate") String end) {
+		ArrayList<Appointment> appointmentList;
+		try {
+			appointmentList = new ArrayList<>(fp.loadAppointments());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return appointmentList = new ArrayList<Appointment>();
+		}
+		appointmentList.stream().filter(appointment -> appointment.getStart().after(new Date(start)));
+		appointmentList.stream().filter(appointment -> appointment.getStart().before(new Date(end)));
+		return appointmentList;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/create")
 	public AppointmentModel createAppointment(@RequestBody AppointmentModel appointment){
-		// der Code
+		// 
 		return appointment;
 	}
 }
